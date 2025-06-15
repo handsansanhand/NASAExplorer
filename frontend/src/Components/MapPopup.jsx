9742a3e874364f35344c86d30c03918f86d37136
handsansanhand <102315833+handsansanhand@users.noreply.github.com>
2025-06-15 17:43:19 +0100

    retrieving more information about certain id made available

--- frontend/src/Components/MapPopup.jsx
+++ frontend/src/Components/MapPopup.jsx
@@ -1,20 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Modal } from 'react-bootstrap';
 import CustomButton from './CustomButton';
+import { retrieveEventByID } from '../Scripts/retrieveEventByID';
 
+//a popup which when called, uses a get request with the event data id to retrieve all the information about the event
 function MapPopup( { show, onHide, eventData } ) {
-      if (!eventData) return null; //just in case
+    if (!eventData) return null; //just in case
+    const [event, setEvent] = useState(null);
+
+    useEffect(() => {
+          if (show && eventData && eventData.id) {
+    const fetchEvent = async () => {
+      try {
+        const fetchedEvent = await retrieveEventByID(eventData.id);
+        setEvent(fetchedEvent);
+      } catch (error) {
+        console.error("Error fetching event:", error);
+        setEvent(null);
+      }
+    };
+    fetchEvent();
+  } else {
+    //when popup closed or no eventData, reset event state
+    setEvent(null);
+  }
+}, [show, eventData]);
+
     return (
         <>
         <Modal show={show} onHide={onHide} centered size="lg">
         <Modal.Header>
-          <Modal.Title>test</Modal.Title>
+          <Modal.Title>{event ? event.title : ''}</Modal.Title>
         </Modal.Header>
         <Modal.Body>
               <div className="modal-body">
                 <img src={`backup_space_pic.jpg`} alt={`test`} />
                 <div className="modal-text">
-                  <p>{`desc`}</p>
+                  <p>{event ? event.description : ''}</p>
                   <div className="author"></div>
                 </div>
               </div>
