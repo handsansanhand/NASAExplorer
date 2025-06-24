fe25fcde918c06bd57b7939476175f7404dcb1d3
handsansanhand <102315833+handsansanhand@users.noreply.github.com>
2025-06-24 22:43:12 +0100

    config changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 //the url will either be the BACKEND_URL (defined in vercel) or if its null, use the local url
-const URL = process.env.BACKEND_URL || "http://localhost:3000";
+const URL = process.env.VITE_BACKEND_URL || "http://localhost:3000";
 
 export default defineConfig({
   base: "./",
